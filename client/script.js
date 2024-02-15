89c98b36ecb2952360094beeca6d3f47812d00c7
Felix Man <felixwhman@gmail.com>
2024-02-14 21:46:14 -0500

    Updated Functions to display products function displayProducts correctly fetches products from api created forEach method iterating over each product fetched and appended to a container Will properly populate and show on site Added DOMcontentloaded event listener calling displayproducts function when webpage is loaded Added modals prompt box allowing users to input personal information to be contacted

--- client/script.js
+++ client/script.js
@@ -1,15 +1,14 @@
-apiUrl = 'http://localhost:3003'
-
 // Create Product class
 class ProductManager {
 	//Initialize with an empty array
 	constructor() {
 		this.products = [] // hold all fetched products within an array
+		this.apiUrl = 'http://localhost:3003/api/products'
 	}
 
 	async getProducts() {
 		try {
-			const response = await axios.get(`${this.apiUrl}/api/products`)
+			const response = await axios.get(this.apiUrl)
 			this.products = response.data // referring to the instance of the class
 		} catch (error) {
 			console.error('Failed to get products.', error)
@@ -17,40 +16,62 @@ class ProductManager {
 	}
 }
 
-// Create instance of class to be used
-const productManager = new ProductManager(apiUrl)
+// Create instance(object) of class to be used
+const productManager = new ProductManager()
 
-// Define function to display products
-const productsPage = document.getElementById('productsButton')
-function displayProducts(products) {
-	const container = document.getElementById('productsContainer') //Get element where products will be displayed
-	container.innerHTML = '' //clear current content to show products
+async function displayProducts() {
+	await productManager.getProducts() // Fetch products from API
+	const products = productManager.products // Fetched products stored in the instance
+
+	const listContainer = document.querySelector('.list') // Products displayed on this HTML element
+	// Clear the listContainer before adding new product cards
+	listContainer.innerHTML = ''
+
+	// iterate over each product in the array
 	products.forEach((product) => {
-		//Create new div element for the product
-		const productElement = document.createElement('div')
-		productElement.textContent = `${product.name} - ${product.brand}` //set text content showing the products name and brand
-		productElement.classList.add('product')
-		container.appendChild(productElement)
+		// Create a new card element for each product
+		const card = document.createElement('div')
+		card.className = 'card'
+
+		// Create and set the image container
+		const imageContainer = document.createElement('div')
+		imageContainer.className = 'image-container'
+		const image = document.createElement('div')
+		image.className = 'image'
+		image.style.backgroundImage = `url(${product.image})`
+		imageContainer.appendChild(image)
+
+		// Create text content container
+		const textContent = document.createElement('div')
+		textContent.className = 'text-content'
+
+		// Insert product name
+		const productName = document.createElement('div')
+		productName.textContent = product.name
+		textContent.appendChild(productName)
+
+		// Append elements to the card
+		card.appendChild(imageContainer)
+		card.appendChild(textContent)
+
+		// Append card to the list container
+		listContainer.appendChild(card)
 	})
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+// Event listiner for when DOM content is loaded
+document.addEventListener('DOMContentLoaded', async () => {
+	await productManager.getProducts()
+	displayProducts()
 	let elems = document.querySelectorAll('.modal')
 
 	// Initialize all modals with options, including the onCloseEnd option
 	let instances = M.Modal.init(elems, {
 		onCloseEnd: function () {
 			// This function will be called when any modal is closed
-			let whenModalCloses = 'We will contact you as soon as possible!'
-			alert(whenModalCloses)
+			let onModalClose = 'We will contact you as soon as possible!'
+			alert(onModalClose)
 		},
-		dismissible: true,
+		dismissible: true, // able to exit by keyboard or overlay click
 	})
-})
-
-// Load products on page load
-document.addEventListener('DOMContentLoaded', async () => {
-	await productManager.getProducts()
-	console.log('Products loaded!')
-	displayProducts(productManager.products)
 })
